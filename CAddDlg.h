6582ce872d3542be34cf797d9ce008428fee105a
雅平 姚 <anhui19156202135@qq.com>
2024-04-15 11:31:02 +0800

    list

---
+++ CAddDlg.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// CAddDlg 对话框
+
+class CAddDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CAddDlg)
+
+public:
+	CAddDlg(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CAddDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOGadd };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CString xuehao;
+	CString name;
+	CString sex;
+
+	afx_msg void OnBnClickedOk();//确定键的响应函数
+};
---
+++ listcontrl.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// listcontrl 对话框
+
+class listcontrl : public CDialogEx
+{
+	DECLARE_DYNAMIC(listcontrl)
+
+public:
+	listcontrl(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~listcontrl();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl m_StuInformation;
+	CString Str_StuId, Str_Name, Str_Gene;
+	int list_row;
+	CMenu m_Menu;
+	  virtual BOOL OnInitDialog();
+	  afx_msg void On32773charu();
+	 // afx_msg void On32774tianjia();
+	  afx_msg void On32778xiugai();
+	  afx_msg void Onmenushanchu();
+};
